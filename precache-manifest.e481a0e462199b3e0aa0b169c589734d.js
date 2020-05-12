fd7de3a1de384b8adddff0a3f66ffaec4bb27943
marcocauu <marco.auu@live.com>
2020-05-11 21:02:58 -0700

    Updates

---
+++ precache-manifest.e481a0e462199b3e0aa0b169c589734d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11af3abc7fd862f129ec11ed31e777c",
+    "url": "/marcocauu/marco-au-resume/index.html"
+  },
+  {
+    "revision": "26b7e06687b07cbbb4f0",
+    "url": "/marcocauu/marco-au-resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a15ef176c4c746fc26a8",
+    "url": "/marcocauu/marco-au-resume/static/js/2.5383874c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marcocauu/marco-au-resume/static/js/2.5383874c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b7e06687b07cbbb4f0",
+    "url": "/marcocauu/marco-au-resume/static/js/main.22d9aed0.chunk.js"
+  },
+  {
+    "revision": "333d65bafcc1fa2be994",
+    "url": "/marcocauu/marco-au-resume/static/js/runtime-main.6b2b5902.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marcocauu/marco-au-resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/marcocauu/marco-au-resume/precache-manifest.e481a0e462199b3e0aa0b169c589734d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marcocauu/marco-au-resume/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
