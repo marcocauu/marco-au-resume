0ce0d3d543691fbdfa721feeb69fccbd7dd86bbb
marcocauu <marco.auu@live.com>
2020-06-03 17:19:33 -0700

    change format for consistency

--- src/helpers/windowOpen.js
+++ src/helpers/windowOpen.js
@@ -1,4 +1,3 @@
 // Factory function for creating functions for window.open
-export const windowOpen = (link, isTargetBlank = false) => () => {
+export const windowOpen = (link, isTargetBlank = false) => () =>
   window.open(link, isTargetBlank ? "_blank" : "");
-};
